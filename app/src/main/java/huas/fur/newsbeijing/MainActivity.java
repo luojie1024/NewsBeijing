83a26c64dab3f6133b99987b245ec656ac81ea6e
luojie1024 <luojie199403@126.com>
2017-02-13 15:54:55 +0800

    Fragments

--- app/src/main/java/huas/fur/newsbeijing/MainActivity.java
+++ app/src/main/java/huas/fur/newsbeijing/MainActivity.java
@@ -1,14 +1,70 @@
 package huas.fur.newsbeijing;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import huas.fur.newsbeijing.fragment.HomeFragment;
+import huas.fur.newsbeijing.fragment.NewsFragment;
+import huas.fur.newsbeijing.fragment.ServiceFragment;
+import huas.fur.newsbeijing.fragment.SettingFragment;
 
 public class MainActivity extends Activity {
+
+          @ViewInject(R.id.main_bottom_rg)
+          RadioGroup rg;
 
           @Override
           protected void onCreate(Bundle savedInstanceState) {
                     super.onCreate(savedInstanceState);
                     setContentView(R.layout.activity_main);
-                    System.out.println("111");
+                    ViewUtils.inject(this);
+                    fragments.add(new HomeFragment());
+                    fragments.add(new NewsFragment());
+                    fragments.add(new ServiceFragment());
+                    fragments.add(new SettingFragment());
+
+                    rg.setOnCheckedChangeListener(checedListener);
+//                    1
+//                    getFragmentManager().
+//                         beginTransaction().
+//                         replace(R.id.main_fragment_container,fragment).
+//                         commit();
+//                    2
+//                    checedListener.onCheckedChanged(rg,R.id.main_home_rb);
+//                    3
+                    ((RadioButton)rg.getChildAt(0)).setChecked(true);
+
           }
+
+          List<Fragment> fragments=new ArrayList<>();
+
+          private RadioGroup.OnCheckedChangeListener checedListener=new RadioGroup.OnCheckedChangeListener() {
+                    /***
+                     *
+                     * @param radioGroup  设置监听的RadioGroup
+                     * @param i             选中的RadioButton的id
+                     */
+                    @Override
+                    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                              View child=radioGroup.findViewById(i);
+                              int index = radioGroup.indexOfChild(child);
+                              Fragment fragment = fragments.get(index);
+
+                              getFragmentManager().
+                                   beginTransaction().
+                                   replace(R.id.main_fragment_container,fragment).
+                                   commit();
+
+                    }
+          };
 }
---
+++ app/src/main/java/huas/fur/newsbeijing/fragment/NewsFragment.java
@@ -0,0 +1,24 @@
+package huas.fur.newsbeijing.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import huas.fur.newsbeijing.R;
+
+/**
+ * 作者：Jacky
+ * 邮箱：550997728@qq.com
+ * 时间：2017/2/13 15:18
+ */
+public class NewsFragment extends Fragment {
+          @Nullable
+          @Override
+          public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+                    View rootView=inflater.inflate(R.layout.fragment_news,null);
+                    return rootView;
+          }
+}
